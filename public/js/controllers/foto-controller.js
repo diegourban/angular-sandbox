9b4d31adb2cf28994e7fa08014b3842839275adf
Diego Leonardo Urban <diego.urban88@gmail.com>
2017-01-18 18:37:04 -0200

    remove and edit photos

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,18 +1,42 @@
-angular.module('alurapic').controller('FotoController', function($scope, $http) {
+angular.module('alurapic').controller('FotoController', function($scope, $http, $routeParams) {
+	
 	$scope.foto = {};
 	$scope.mensagem = "";
 
+	if($routeParams.fotoId) {
+		$http.get('v1/fotos/' + $routeParams.fotoId)
+		.success(function(foto) {
+			$scope.foto = foto;
+		})
+		.error(function(err) {
+			console.log(err);
+			$scope.mensagem = "Não foi possível obter a foto";
+		});
+	}
+
 	$scope.submeter = function() {
 		if($scope.formulario.$valid) {
-			$http.post('v1/fotos', $scope.foto)
-			.success(function() {
-				$scope.foto = {};
-				$scope.mensagem = 'Foto incluida com sucesso';
-			})
-			.error(function(err) {
-				$scope.mensagem = 'Não foi possível incluir a foto';
-				console.log(err);
-			});	
+			if($scope.foto._id) {
+				$http.put('v1/fotos/' + $scope.foto._id, $scope.foto)
+				.success(function() {
+					$scope.foto = {};
+					$scope.mensagem = 'Foto alterada com sucesso';
+				})
+				.error(function(err) {
+					$scope.mensagem = 'Não foi possível alterar a foto ' + $scope.foto.titulo;
+					console.log(err);
+				});
+			} else {
+				$http.post('v1/fotos', $scope.foto)
+				.success(function() {
+					$scope.foto = {};
+					$scope.mensagem = 'Foto incluida com sucesso';
+				})
+				.error(function(err) {
+					$scope.mensagem = 'Não foi possível incluir a foto';
+					console.log(err);
+				});
+			}
 		}
 	}
 });
