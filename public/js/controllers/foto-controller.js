50d26e1745af219236f117847d2a439021d07c00
Diego Leonardo Urban <diego.urban88@gmail.com>
2017-01-19 20:44:24 -0200

    adding picture manager

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,4 +1,4 @@
-angular.module('alurapic').controller('FotoController', function($scope, $routeParams, recursoFoto) {
+angular.module('alurapic').controller('FotoController', function($scope, recursoFoto, $routeParams, cadastroDeFotos) {
 	
 	$scope.foto = {};
 	$scope.mensagem = "";
@@ -14,22 +14,14 @@ angular.module('alurapic').controller('FotoController', function($scope, $routeP
 
 	$scope.submeter = function() {
 		if($scope.formulario.$valid) {
-			if($scope.foto._id) {
-				recursoFoto.update({fotoId : $scope.foto._id}, $scope.foto, function() {
-					$scope.mensagem = 'Foto alterada com sucesso';
-				}, function(err) {
-					console.log(err);
-					$scope.mensagem = 'Não foi possível alterar a foto ' + $scope.foto.titulo;
-				});
-			} else {
-				recursoFoto.save($scope.foto, function() {
-					$scope.foto = {};
-					$scope.mensagem = 'Foto incluida com sucesso';
-				}, function(err){
-					console.log(err);
-					$scope.mensagem = 'Não foi possível incluir a foto';
-				});
-			}
+			cadastroDeFotos.cadastrar($scope.foto)
+			.then(function(dados) {
+				$scope.mensagem = dados.mensagem;
+				if($scope.inclusao) $scope.foto = {};
+			})
+			.catch(function(dados) {
+				$scope.mensagem = dados.mensagem;
+			});
 		}
 	}
 });
--- public/js/services/meus-servicos.js
+++ public/js/services/meus-servicos.js
@@ -1,10 +1,43 @@
 angular.module('meusServicos', ['ngResource'])
 .factory('recursoFoto', function($resource) {
-
 	return $resource('v1/fotos/:fotoId', null, {
 		update: {
 			method: 'PUT'
 		}
 	});
+})
+.factory('cadastroDeFotos', function(recursoFoto, $q) {
+	var servico = {};
 
+	servico.cadastrar = function(foto) {
+		return $q(function(resolve, reject) {
+			if(foto._id) {
+				recursoFoto.update({fotoId : foto._id}, foto, function() {
+					resolve({
+						mensagem : 'Foto ' + foto.titulo + ' atualizada com sucesso!',
+						inclusao : false
+					});
+				}, function(err) {
+					console.log(err);
+					reject({
+						mensagem : 'Não foi possível atualizar a foto ' + foto.titulo
+					});
+				});
+			} else {
+				recursoFoto.save(foto, function() {
+					resolve({
+						mensagem : 'Foto ' + foto.titulo + ' incluída com sucesso!',
+						inclusao : true
+					})
+				}, function(err) {
+					console.log(err);
+					reject({
+						mensagem : 'Não foi possível incluir a foto ' + foto.titulo
+					});
+				});
+			}
+		});
+	}
+
+	return servico;
 });
